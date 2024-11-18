6a1b072626a3e22cba4a08cec5a2e721452de0b7
JuliaSPalmeira <serennamares@gmail.com>
2024-11-18 18:05:54 -0300

    estudos js

---
+++ aula12/ex013.js
@@ -0,0 +1,41 @@
+var agora = new Date()
+var diaSem = agora.getDay()
+
+console.log(diaSem)
+
+switch(diaSem) {
+    case 0:
+        console.log('Domingo')
+        break
+    case 1:
+        console.log('Segunda')
+        break
+    case 2:
+       console.log('Terça')
+        break
+    case 3:
+        console.log('Quarta')
+        break
+    case 4:
+        console.log('Quinta')
+        break
+    case 5:
+        console.log('Sexta')
+        break
+    case 6:
+        console.log('Sabado')
+        break
+    default: 
+    console.log('[erro]')    
+}
+
+/*
+0 =  dom
+1 = seg
+2 = ter
+3 = qua
+4 = qui
+5 = sex
+6 = sab
+
+*/
