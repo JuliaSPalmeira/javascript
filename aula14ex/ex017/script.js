d7d95e1fe556ad6014cc498d2f39d2394e29ec6d
JuliaSPalmeira <serennamares@gmail.com>
2024-11-21 19:47:01 -0300

    ex 02

--- aula14ex/ex017/script.js
+++ aula14ex/ex017/script.js
@@ -1,1 +1,19 @@
-alert('olá')
+function gerar(){
+    let num = window.document.getElementById('txtn')
+    let tab = window.document.getElementById('seltab')
+
+    if (num.value.length == 0){
+        window.alert('Por Favor! Digite um número:')
+    } else{
+        let n = Number(num.value)
+        tab.innerHTML = ''
+       for (c =1; c<=10 ;c++){
+        let item = document.createElement('option')
+        item.text = `${n} x ${c} = ${n*c}`
+        item.value = `tab${c}`
+        tab.appendChild(item)
+       }
+    }
+
+
+}
